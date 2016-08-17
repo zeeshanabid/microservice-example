3d686a01291e4e0f46ba3f2995a53e4a661413ad
Zeeshan Abid <zeeshan.abid@gmail.com>
2016-08-17 11:20:30 +0200

    Install nats on provision

--- provision.sh
+++ provision.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
 apt-get update
+apt-get -y upgrade
+apt-get install -y ruby2.0 ruby2.0-dev build-essential
+gem2.0 install nats
 curl -sSL https://get.docker.com/ | sh
 usermod -aG docker vagrant
 curl -L https://github.com/docker/compose/releases/download/1.8.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
